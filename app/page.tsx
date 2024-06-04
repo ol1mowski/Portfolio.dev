ad5f9043b707e951910ca396c5ec26fe269a0208
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-04 14:38:43 +0200

    [AboutMe] component: introduce aboutMe component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/UI/Header/Header.component";
 import HomePage from "@/components/pages/HomePage/HomePage.page";
+import About from "@/components/pages/about/About.component";
 
 export default function Home() {
   return (
     <>
       <Header />
       <HomePage />
+      <About />
     </>
   );
 }
