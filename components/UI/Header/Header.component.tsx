519afe0c692dbbec0bccbad5ef33b4f9dd4d1694
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-04 12:24:23 +0200

    [Header] UI: improve UI at Hamburger Nav Section

--- components/UI/Header/Header.component.tsx
+++ components/UI/Header/Header.component.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import s from "./Header.component.module.scss";
 
@@ -8,6 +8,7 @@ import hamburger from "@/assets/hamburger.png";
 import { useRef } from "react";
 import NavBar from "../NavBar/NavBar.component";
 import Image from "next/image";
+import MenuItem from "../MenuItem/MenuItem.component";
 
 const Header = () => {
   const menu = useRef<HTMLDivElement>(null);
@@ -24,12 +25,33 @@ const Header = () => {
     }
   };
 
+  const menuItems = [
+    { to: "home", label: "Home" },
+    { to: "about", label: "O mnie" },
+    { to: "projects", label: "Projekty" },
+    { to: "/", label: "Blog" },
+    { to: "contact", label: "Kontakt" },
+  ];
+
+  const buttonVariants = {
+    hover: {
+      scale: 1.05,
+      transition: {
+        duration: 0.3,
+        yoyo: Infinity,
+      },
+    },
+  };
+
   return (
     <>
       <header className={s.headerContainer}>
         <section className={s.headerContainer__nameSection}>
           <h3 className={s.headerContainer__nameSection__content}>
-            <span className={s.headerContainer__nameSection__content__name}>Oliwier</span>.dev
+            <span className={s.headerContainer__nameSection__content__name}>
+              Oliwier
+            </span>
+            .dev
           </h3>
         </section>
         <nav className={s.headerContainer__navSection}>
@@ -54,13 +76,24 @@ const Header = () => {
       >
         <div className={s.headerContainer__menu__x}>
           <Image
+            width={30}
+            height={30}
             src={x}
             alt="close icon"
             className={s.headerContainer__menu__x__icon}
           />
         </div>
         <section className={s.headerContainer__menu__nav}>
-          <NavBar />
+          {menuItems.map((item, index) => (
+            <MenuItem
+              key={index}
+              to={item.to}
+              label={item.label}
+              index={index + 1}
+              buttonVariants={buttonVariants}
+              hideMenu={hideMenuHandler}
+            />
+          ))}
         </section>
       </nav>
     </>
