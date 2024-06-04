08f0492e7d8b093a19f6458a82c65a2dc2c612b7
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-04 14:38:43 +0200

    [AboutMe] component: introduce aboutMe component

---
+++ components/UI/AnimationWrapper/AnimationWrapper.component.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { type ReactNode } from "react";
+import { useInView } from "react-intersection-observer";
+
+type AnimationWrapperProps = {
+    children: ReactNode;
+    className?: string;
+}
+
+function AnimationWrapper({ children, className }: AnimationWrapperProps) {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
+  const animationVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5, ease: "easeOut" },
+    },
+  };
+  return (
+    <motion.section
+      ref={ref}
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+      variants={animationVariants}
+      className={className}
+    >
+        { children }
+    </motion.section>
+  );
+}
+
+export default AnimationWrapper;
---
+++ components/pages/about/About.component.tsx
@@ -0,0 +1,56 @@
+import style from "./About.component.module.scss";
+import about from "@/assets/author_about.svg";
+import AnimationWrapper from "@/components/UI/AnimationWrapper/AnimationWrapper.component";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <AnimationWrapper>
+      <div id="about" />
+      <section className={style.aboutContainer}>
+        <div className={style.aboutContainer__imageSection}>
+          <Image
+            width={600}
+            height={600}
+            src={about}
+            alt="work place"
+            className={style.aboutContainer__imageSection__img}
+          />
+        </div>
+        <div className={style.aboutContainer__contentSection}>
+          <h3 className={style.aboutContainer__contentSection__h3}>O Mnie</h3>
+          <h4 className={style.aboutContainer__contentSection__h4}>
+            Nazywam się Oliwier Markiewicz,
+            <br /> mieszkam w Sieradzu, Polska📍
+          </h4>
+          <p className={style.aboutContainer__contentSection__p}>
+            Zajmuję się projektowaniem oraz tworzeniem nowoczesnych i
+            funkcjonalnych stron internetowych w technologiach takich jak
+            Wordpress, Next.js czy React, które spełniają indywidualne potrzeby
+            klientów.
+          </p>{" "}
+          <p className={style.aboutContainer__contentSection__p}>
+            Pomagam małym firmą zaistnieć w internecie, oferując kompleksowe
+            rozwiązania obejmujące wdrażanie strategii marketingowych, które
+            zwiększają widoczność i przyciągają nowych klientów.
+          </p>
+          <p className={style.aboutContainer__contentSection__p}>
+            Prowadzę równie kanał na{" "}
+            <a
+              href="https://www.youtube.com/channel/UCTNFKRALTZoSQS6mDOuDs2Q"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className={style.aboutContainer__contentSection__p__yt}>
+                YouTube
+              </span>
+            </a>{" "}
+            gdzie dzielę się wiedzą z zakresu programowania stron internetowych
+          </p>
+        </div>
+      </section>
+    </AnimationWrapper>
+  );
+};
+
+export default About;
