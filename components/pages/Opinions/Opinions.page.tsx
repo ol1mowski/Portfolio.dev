06295e05036ce3832ff5123c322febd8fdd980d9
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-08 13:35:21 +0200

    [Opinions] background: add style to background {images and display}

--- components/pages/Opinions/Opinions.page.tsx
+++ components/pages/Opinions/Opinions.page.tsx
@@ -1,12 +1,29 @@
-import s from './Opinions.page.module.scss';
+import s from "./Opinions.page.module.scss";
 
+import rec_1 from "@/assets/Rectangle_1.png";
+import rec_2 from "@/assets/Rectangle_2.png";
+import Image from "next/image";
 
 function Opinions() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className={s.container}>
+      <Image
+        height={700}
+        src={rec_1}
+        alt="Reactangel"
+        className={s.container__rec1}
+      />
+      <Image
+        height={700}
+        src={rec_2}
+        alt="Reactangel"
+        className={s.container__rec2}
+      />
+      <section className={s.container__boxWrapper}>
+        <h2>Zobacz Co myślą o mnie moi Klienci</h2>
+      </section>
+    </section>
+  );
 }
 
-export default Opinions
+export default Opinions;
