a28bd3369b3dc35859a7381ac56892b2525b8a29
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-09 09:05:59 +0200

    [Opinions] style: adjust RWD in component

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com"],
+    remotePatterns: [
+      {
+        hostname: "firebasestorage.googleapis.com",
+      },
+      // {
+      //   hostname: "res.cloudinary.com",
+      // },
+    ],
   },
 };
 
