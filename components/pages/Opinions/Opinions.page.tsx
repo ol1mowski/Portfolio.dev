91a7f4c6843c91057cee2e8d401e6f0fc32b64e7
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-09 16:01:04 +0200

    [Opinions] comapnyCard: styling compamny card {!RWD to do}

--- components/pages/Opinions/Opinions.page.tsx
+++ components/pages/Opinions/Opinions.page.tsx
@@ -2,6 +2,8 @@ import s from "./Opinions.page.module.scss";
 
 import rec_1 from "@/assets/Rectangle_1.png";
 import rec_2 from "@/assets/Rectangle_2.png";
+import companyLogo from "@/assets/Infmovilweb.jpeg";
+
 import Image from "next/image";
 
 function Opinions() {
@@ -25,9 +27,22 @@ function Opinions() {
           Zobacz Co myślą o mnie moi{" "}
           <span className={s.container__boxWrapper__header__mark}>Klienci</span>{" "}
         </h2>
-      <section className={s.container__boxWrapper__opinionContainer}>
-
-      </section>
+        <section className={s.container__boxWrapper__opinionContainer}>
+          <Image
+            height={200}
+            width={200}
+            src={companyLogo}
+            alt="company logo"
+            className={s.container__boxWrapper__opinionContainer__companyLogo}
+          />
+          <span
+            className={
+              s.container__boxWrapper__opinionContainer__read
+            }
+          >
+            Przeczytaj Rekomendacje
+          </span>
+        </section>
       </section>
     </section>
   );
