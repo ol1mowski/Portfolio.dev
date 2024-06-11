d34624cb019640c71b4a37ee51ea43372c63dbb1
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-11 16:17:44 +0200

    [404] link: add valid link to homepage and remove footer from layout

--- components/pages/Blog/NotFound/NotFound.page.tsx
+++ components/pages/Blog/NotFound/NotFound.page.tsx
@@ -2,11 +2,11 @@ import Caption from "@/components/UI/Caption/Caption.component";
 import Link from "next/link";
 import s from './NotFound.page.module.scss';
 
-function NotFound() {
+function NotFound({ link }: { link: string}) {
   return (
     <section className={s.container}>
-      <Caption type="sub" value="404 - Nie znaleniono strony" />
-      <Link href={"/"} className={s.container__link}>Przejdź do strony głównej</Link>
+      <Caption type="sub" value="404 - Nie znaleniono strony" /> <br />
+      <Link href={link} className={s.container__link}>Przejdź do strony głównej</Link>
     </section>
   );
 }
