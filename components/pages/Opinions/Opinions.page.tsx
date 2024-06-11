fb4a1a049963f45928d74465d7809dac98f8cc61
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-11 10:54:04 +0200

    [Opinions]  Link: add link to recomendation

--- components/pages/Opinions/Opinions.page.tsx
+++ components/pages/Opinions/Opinions.page.tsx
@@ -9,54 +9,66 @@ import Image from "next/image";
 
 function Opinions() {
   return (
-    <section id="opinions" className={s.container}>
-      <Image
-        height={700}
-        width={600}
-        src={rec_1}
-        alt="Reactangel"
-        className={s.container__rec1}
-      />
-      <section className={s.container__boxWrapper}>
-        <section className={s.container__boxWrapper__contentSection}>
-          <h3 className={s.container__boxWrapper__contentSection__h3}>
-            OPINIE
-          </h3>
-          <h4 className={s.container__boxWrapper__contentSection__h4}>
-            Zobacz Co Myślą O Mnie Moi Klienci
-          </h4>
-          <section className={s.container__boxWrapper__opinionContainer}>
-            <Image
-              width={300}
-              height={200}
-              className={s.container__boxWrapper__opinionContainer__img}
-              src={companyLogo}
-              alt="company logo"
-            />
+    <a href="https://jmp.sh/s/mNlcSWPwdNWRNFOaei2n" target="_blank">
+      <section id="opinions" className={s.container}>
+        <Image
+          height={700}
+          width={600}
+          src={rec_1}
+          alt="Reactangel"
+          className={s.container__rec1}
+        />
+        <section className={s.container__boxWrapper}>
+          <section className={s.container__boxWrapper__contentSection}>
+            <h3 className={s.container__boxWrapper__contentSection__h3}>
+              OPINIE
+            </h3>
+            <h4 className={s.container__boxWrapper__contentSection__h4}>
+              Zobacz Co Myślą O Mnie Moi Klienci
+            </h4>
+            <section className={s.container__boxWrapper__opinionContainer}>
+              <Image
+                width={300}
+                height={200}
+                className={s.container__boxWrapper__opinionContainer__img}
+                src={companyLogo}
+                alt="company logo"
+              />
 
-            <section
-              className={s.container__boxWrapper__opinionContainer__readWrapper}
-            >
-              <span
+              <section
                 className={
-                  s.container__boxWrapper__opinionContainer__readWrapper__text
+                  s.container__boxWrapper__opinionContainer__readWrapper
                 }
               >
-                Przeczytaj{" "}
                 <span
                   className={
-                    s.container__boxWrapper__opinionContainer__readWrapper__text__mark
+                    s.container__boxWrapper__opinionContainer__readWrapper__text
                   }
                 >
-                  Opinię
+                  Przeczytaj{" "}
+                  <span
+                    className={
+                      s.container__boxWrapper__opinionContainer__readWrapper__text__mark
+                    }
+                  >
+                    Opinię
+                  </span>
                 </span>
-              </span>
-              <Image className={s.container__boxWrapper__opinionContainer__readWrapper__icon} width={18} height={18} src={arrow} alt="Arrow Icon" />
+                <Image
+                  className={
+                    s.container__boxWrapper__opinionContainer__readWrapper__icon
+                  }
+                  width={18}
+                  height={18}
+                  src={arrow}
+                  alt="Arrow Icon"
+                />
+              </section>
             </section>
           </section>
         </section>
       </section>
-    </section>
+    </a>
   );
 }
 
