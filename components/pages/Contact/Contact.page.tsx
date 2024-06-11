cb3e8b64bbf8cccab940680e6c13249c517a7904
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-11 10:56:59 +0200

    [Location] content: change content ad location section

--- components/pages/Contact/Contact.page.tsx
+++ components/pages/Contact/Contact.page.tsx
@@ -51,7 +51,7 @@ const Contact = () => {
                         style.contactContainer__wrapper__icons__icon__info__h3
                       }
                     >
-                      Location
+                      Lokalizacja
                     </h3>
                     <a
                       href="https://www.google.com/maps/place/Sieradz/@51.6016962,18.6973771,13z/data=!3m1!4b1!4m6!3m5!1s0x471af589786a817f:0x29bb12c99326d34e!8m2!3d51.5956014!4d18.7302994!16zL20vMDIyOG1o?hl=pl-PL&entry=ttu"
@@ -66,7 +66,7 @@ const Contact = () => {
                           style.contactContainer__wrapper__icons__icon__info__p
                         }
                       >
-                        Sieradz, Poland
+                        Sieradz, Polska
                       </p>
                     </a>
                   </div>
