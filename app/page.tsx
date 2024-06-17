e6b634ee6d8d7e22652e5d21ad538fac468e6b44
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-17 12:41:33 +0200

    [About] section: fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "@/components/UI/Header/Header.component";
 import HomePage from "@/components/pages/HomePage/HomePage.page";
 
-import About from "@/components/pages/About/About.page";
+
 import Projects from "@/components/pages/Projects/Projects.page";
 import PostsAmount from "@/components/pages/PostsAmount/PostsAmount.page";
 import SubscribersAmount from "@/components/pages/SubscribersAmount/SubscribersAmount.page";
 import Contact from "@/components/pages/Contact/Contact.page";
 import Footer from "@/components/pages/Footer/Footer.page";
 import Opinions from "@/components/pages/Opinions/Opinions.page";
+import About from "@/components/pages/About/About.page";
 
 export default function Home() {
   return (
