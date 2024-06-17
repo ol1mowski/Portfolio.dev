5db0e31a3ec60926c8e1c71123975188bfbd3c69
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-06-17 15:52:45 +0200

    [root] sitemap: add site map

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "firebasestorage.googleapis.com",
-      },
-      {
         hostname: "res.cloudinary.com",
       },
     ],
