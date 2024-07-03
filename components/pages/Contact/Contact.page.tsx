3b2ac4da27e6f05abfe71696b13fc7097df49a54
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-07-03 18:15:07 +0200

    [Contact] new icon: add new telephone icon

--- components/pages/Contact/Contact.page.tsx
+++ components/pages/Contact/Contact.page.tsx
@@ -3,7 +3,6 @@ import style from "./Contact.page.module.scss";
 import ContactInfoSection from "./ContactInfoSection/ContactInfoSection.component";
 import ContectHeaderSection from "./ContectHeaderSection/ContectHeaderSection.component";
 
-
 const Contact = () => {
   return (
     <>
@@ -11,17 +10,20 @@ const Contact = () => {
         <div className={style.contactContainer__wrapper}>
           <ContectHeaderSection />
           <div className={style.contactContainer__wrapper__icons}>
-
             <ContactInfoSection
-              link="https://www.google.com/maps/place/Sieradz/@51.6016962,18.6973771,13z/data=!3m1!4b1!4m6!3m5!1s0x471af589786a817f:0x29bb12c99326d34e!8m2!3d51.5956014!4d18.7302994!16zL20vMDIyOG1o?hl=pl-PL&entry=ttu"
-              title="Lokalizacja"
-              content="Sieradz, Polska"
+              link="tel: +48 693 851 878"
+              title="Telefon"
+              content="+48 693 851 878"
             />
-
             <ContactInfoSection
               link="mailto:oliwier.markiewicz.dev@gmail.com"
               title="Mail"
               content="oliwier.markiewicz.dev@gmail.com"
+            />
+            <ContactInfoSection
+              link="https://www.google.com/maps/place/Sieradz/@51.6016962,18.6973771,13z/data=!3m1!4b1!4m6!3m5!1s0x471af589786a817f:0x29bb12c99326d34e!8m2!3d51.5956014!4d18.7302994!16zL20vMDIyOG1o?hl=pl-PL&entry=ttu"
+              title="Lokalizacja"
+              content="Sieradz, Polska"
             />
           </div>
         </div>
