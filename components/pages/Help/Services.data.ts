52a4d5e6337810025bf880bb45d3df93708ad40d
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-07-03 15:09:19 +0200

    [Services] optimalize: optymalze and refactoring code in section

---
+++ components/pages/Help/Services.data.ts
@@ -0,0 +1,45 @@
+import website from "@/assets/website.png";
+import marketing from "@/assets/marketing.png";
+import social from "@/assets/social.png";
+import google from "@/assets/google.png";
+
+export const SERVICES = [
+  {
+    id: 1,
+    title: "Strony i Sklpy Internetowe",
+    description: `Tworzę profesjonalne i responsywne strony internetowe oraz sklepy
+        online, które pomagają Twojej firmie zaistnieć w sieci.`,
+    image: website,
+    width: 100,
+    height: 100,
+  },
+  {
+    id: 2,
+    title: "Marketing Internetowy",
+    description: `Przygotowuję kampanie marketingowe dzięki którym przyciągniesz
+            nowych klientów i zwiększysz sprzedaż swoich produktów lub usług.`,
+    image: marketing,
+    width: 64,
+    height: 64,
+  },
+  {
+    id: 3,
+    title: "Prowadzenie Social Media",
+    description: `Dzięki profesjonalnemu zarządzaniu Twoimi profilami
+            społecznościowymi zyskasz większą widoczność i wzmocnisz więź z
+            klientami.`,
+    image: social,
+    width: 64,
+    height: 64,
+  },
+  {
+    id: 4,
+    title: "Optymalizacja Wizytówki Google Moja Firma",
+    description: `Oferuję profesjonalną optymalizację wizytówki Google Moja Firma,
+            która pozwoli Twojej firmie wyróżnić się w wynikach wyszukiwania
+            lokalnego.`,
+    image: google,
+    width: 64,
+    height: 64,
+  },
+];
