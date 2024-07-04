d2dc7ca0bea8a6715b43ebf20232088c4180a757
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-07-04 22:00:08 +0200

    [SubscribersAmount] UI: change look and improve UI

--- components/pages/SubscribersAmount/SubscribersAmount.page.tsx
+++ components/pages/SubscribersAmount/SubscribersAmount.page.tsx
@@ -1,13 +1,53 @@
 import CounterAnimation from "@/components/UI/CounterAnimation/CounterAnimation.component";
 
+import s from "./SubscribersAmount.page.module.scss";
+import Link from "next/link";
+
 function SubscribersAmount() {
   return (
-    <CounterAnimation
-      duration={2000}
-      target={6500}
-      description="Wyświetleń na kanale"
-      action="YouTube"
-    />
+    <section className={s.container}>
+      <article className={s.container__media}>
+        <div>
+          <span className={s.container__media__beforeNumber}>+</span>
+          <CounterAnimation duration={2000} target={6500} />
+        </div>
+        <a
+          href="https://www.youtube.com/channel/UCTNFKRALTZoSQS6mDOuDs2Q"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className={s.container__media__description}>
+            Wyświetleń na YouTube
+          </span>
+        </a>
+      </article>
+      <article className={s.container__media}>
+        <div>
+          <span className={s.container__media__beforeNumber}>+</span>
+          <CounterAnimation duration={2000} target={30} />
+        </div>
+        <a
+          href="https://www.youtube.com/channel/UCTNFKRALTZoSQS6mDOuDs2Q"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className={s.container__media__description}>
+            Darmowych Filmów
+          </span>
+        </a>
+      </article>
+      <article className={s.container__media}>
+         <div>
+          <span className={s.container__media__beforeNumber}></span>
+          <CounterAnimation duration={2000} target={10} />
+        </div>
+        <Link href={"/"}>
+          <span className={s.container__media__description}>
+            Darmowych Wpisów Na Blogu
+          </span>
+        </Link>
+      </article>
+    </section>
   );
 }
 
