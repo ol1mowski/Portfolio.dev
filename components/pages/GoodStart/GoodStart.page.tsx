158d0a8e4053570eb2758553ec09e1ebbfb8c081
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-07-21 15:26:31 +0200

    [Good Start] RWD: adjust section to desktop devices

--- components/pages/GoodStart/GoodStart.page.tsx
+++ components/pages/GoodStart/GoodStart.page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import s from "./GoodStart.page.module.scss";
 
 import ebook from "@/assets/bezplatny_ebook.png";
+import AnimationWrapper from "@/components/UI/AnimationWrapper/AnimationWrapper.component";
 
 function GoodStart() {
   return (
-    <section className={s.container}>
+    <AnimationWrapper>
+        <section className={s.container}>
       <section className={s.container__content}>
-         
         <section className={s.container__content__header}>
           <h2 className={s.container__content__header__title}>
             Na Dobry Początek <br /> Naszej Znajomości
@@ -16,17 +17,24 @@ function GoodStart() {
         </section>
         <p className={s.container__content__p}>
           Aby nasza znajomość zaczęła się naprawdę dobrze, chciałbym podzielić
-          się z Tobą moim najnowszym e-bookiem, który otrzymasz zupełnie za
-          darmo !
+          się z Tobą moim najnowszym e-bookiem, który otrzymasz zupełnie{" "}
+          <span className={s.container__content__p__special}>za darmo </span> !
         </p>
         <button className={s.container__content__btn}>
           Odbierz Bezpłatnie
         </button>
       </section>
       <section className={s.container__image}>
-        <Image src={ebook} alt="" width={500} height={600}/>
+        <Image
+          src={ebook}
+          className={s.container__image__img}
+          alt="bezpłatny ebook projektowanie stron internetowych"
+          width={500}
+          height={600}
+        />
       </section>
     </section>
+    </AnimationWrapper>
   );
 }
 
