00ee65b591f3cf42f4bfcb3205ed8c716bd2b716
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-08-11 13:56:29 +0200

    [Ebook] auth: add auth to the form ebook site

--- app/Ebook/page.tsx
+++ app/Ebook/page.tsx
@@ -5,40 +5,50 @@ import Header from "@/components/UI/Header/Header.component";
 import Footer from "@/components/pages/Footer/Footer.page";
 import dynamic from "next/dynamic";
 import { saveClientData } from "@/actions/SaveClientEmail";
+import { validateSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-const ClientForm = dynamic(() => import("./FormComponents/Form.validation.component"), {
-  ssr: false,
-});
+const ClientForm = dynamic(
+  () => import("./FormComponents/Form.validation.component"),
+  {
+    ssr: false,
+  }
+);
 
-function page() {
-  return (
-    <>
-      <Header type="out" />
-      <section className={s.container}>
-        <section className={s.container__content}>
-          <section className={s.container__content__header}>
-            <h2 className={s.container__content__header__title}>
-              Jeszcze Tylko Jeden Krok
-            </h2>
-            <span className={s.container__content__header__line}></span>
+async function page() {
+  const result = await validateSession();
+  if (result.session) {
+    redirect("/Thanks");
+  } else {
+    return(
+      <>
+        <Header type="out" />
+        <section className={s.container}>
+          <section className={s.container__content}>
+            <section className={s.container__content__header}>
+              <h2 className={s.container__content__header__title}>
+                Jeszcze Tylko Jeden Krok
+              </h2>
+              <span className={s.container__content__header__line}></span>
+            </section>
+            <section className={s.container__form}>
+              <ClientForm action={saveClientData} />
+            </section>
           </section>
-          <section className={s.container__form}>
-            <ClientForm action={saveClientData} />
+          <section className={s.container__image}>
+            <Image
+              src={ebook}
+              className={s.container__image__img}
+              alt="bezpłatny ebook projektowanie stron internetowych"
+              width={500}
+              height={600}
+            />
           </section>
         </section>
-        <section className={s.container__image}>
-          <Image
-            src={ebook}
-            className={s.container__image__img}
-            alt="bezpłatny ebook projektowanie stron internetowych"
-            width={500}
-            height={600}
-          />
-        </section>
-      </section>
-      <Footer />
-    </>
-  );
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default page;
