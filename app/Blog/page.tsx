a3c5c63d303b1e125453efcd6f683f003dfb0a41
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-07 20:47:31 +0200

    [Blog] remove components: remove Yt section and Ebook components

--- app/Blog/page.tsx
+++ app/Blog/page.tsx
@@ -1,12 +1,8 @@
+import Head from "next/head";
+
 import Footer from "@/components/UI/FooterBlog/Footer.component.page";
 import HomePage from "@/components/pages/Blog/HomePage/HomePage.fetching";
-// import Newsletter from "@/components/pages/Blog/Newsletter/Newsletter.page";
 import Posts from "@/components/pages/Blog/Posts/Posts.fetching";
-import YTSection from "@/components/pages/Blog/YTSection/YTSection.page";
-import GoodStart from "@/components/pages/GoodStart/GoodStart.page";
-import React from "react";
-import EbookSection from "./Ebook/EbookSection.page";
-import Head from "next/head";
 
 function page() {
   return (
@@ -19,10 +15,7 @@ function page() {
         />
       </Head>
       <HomePage />
-      <EbookSection />
-      <YTSection />
       <Posts />
-      {/* <Newsletter /> */}
       <Footer />
     </>
   );
