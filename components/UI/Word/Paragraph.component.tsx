51b23f0fdbbf4cc8662bc83d6149f5741bc2ab06
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-20 21:02:03 +0200

    [/] UX: create a animation text component

---
+++ components/UI/Word/Paragraph.component.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { MotionValue, motion, useScroll, useTransform } from "framer-motion";
+import { type ReactNode, useRef } from "react";
+
+function Paragraph({ value, className }: { value: string; className: string }) {
+  const element = useRef<HTMLParagraphElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ["start 0.9", "start 0.25"],
+  });
+
+  const words = value.split(" ");
+
+  return (
+    <span ref={element}>
+      {words.map((word, i) => {
+        const start = i / words.length;
+        const end = start + 1 / words.length;
+        return (
+          <Word
+            className={className}
+            key={i}
+            range={[start, end]}
+            progress={scrollYProgress}
+          >
+            {word + " "}
+          </Word>
+        );
+      })}
+    </span>
+  );
+}
+
+export default Paragraph;
+
+const Word = ({
+  children,
+  range,
+  progress,
+  className,
+}: {
+  children: ReactNode;
+  className: string;
+  range: [number, number];
+  progress: MotionValue<number>;
+}) => {
+  const opacity = useTransform(progress, range, [0, 1]);
+  const y = useTransform(progress, range, [20, 0]);
+
+  return (
+    <motion.span className={className} style={{ opacity, y }}>
+      {children}
+    </motion.span>
+  );
+};
