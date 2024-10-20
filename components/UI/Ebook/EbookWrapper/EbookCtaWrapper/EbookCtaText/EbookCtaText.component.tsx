4e77255374f9da522ba357e1f839cf67d0e70a3a
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-20 21:04:23 +0200

    [/] UX: introduce animate text component to E-Book cta section

--- components/UI/Ebook/EbookWrapper/EbookCtaWrapper/EbookCtaText/EbookCtaText.component.tsx
+++ components/UI/Ebook/EbookWrapper/EbookCtaWrapper/EbookCtaText/EbookCtaText.component.tsx
@@ -1,11 +1,16 @@
+import Paragraph from "@/components/UI/Word/Paragraph.component";
 import s from "./EbookCtaText.component.module.scss";
 
 function EbookCtaText() {
   return (
     <section className={s.pWrapper}>
       <p className={s.pWrapper__p}>
-        Aby nasza znajomość zaczęła się naprawdę dobrze, chciałbym podzielić się
-        z Tobą moim najnowszym e-bookiem, który otrzymasz zupełnie{" "}
+        <Paragraph
+          value="Aby nasza znajomość zaczęła się naprawdę dobrze, chciałbym podzielić się
+        z Tobą moim najnowszym e-bookiem, który otrzymasz zupełnie"
+          className={s.pWrapper__p}
+        />
+
         <span className={s.importantTextDecorate}>za darmo !</span>
       </p>
     </section>
