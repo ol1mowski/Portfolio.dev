6da100eaf07db531ee136f22d0bde5b3858e8cec
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-21 09:47:42 +0200

    [/] opinions: add basic style to component [ONLY TO MOBILE DEVICES ]

--- components/pages/Opinions/Opinions.page.tsx
+++ components/pages/Opinions/Opinions.page.tsx
@@ -1,5 +1,6 @@
-import Paragraph from "@/components/UI/Word/Paragraph.component";
 import s from "./Opinions.page.module.scss";
+
+import Paragraph from "@/components/UI/Word/Paragraph.component";
 import Image from "next/image";
 
 function Opinions() {
@@ -18,10 +19,10 @@ function Opinions() {
       </div>
       <div className={s.container__opinionsWrapper}>
             <div className={s.container__opinionsWrapper__opinion}>
-                <p className={s.container__opinionsWrapper__opinion__text}></p>
-                <a href="" target="_blank" rel="noopener noreferrer"><span className={s.container__importantTextYtDecorate}>Zobacz Pełną Opinię</span></a>
+                <p className={s.container__opinionsWrapper__opinion__text}>Oliwier Markiewicz is a good lad and very hardworking. He make us the company website as we instructed. We hope you do well in life.</p>
+                <a href="https://jumpshare.com/s/mNlcSWPwdNWRNFOaei2n" target="_blank" rel="noopener noreferrer"><span className={s.container__importantTextDecorate}>Zobacz Pełną Opinię</span></a>
                 <div className={s.container__opinionsWrapper__opinion__imgWrapper}>
-                    <Image src={''} alt="Company Logo" width={400} height={300}/>
+                    <Image src={'https://res.cloudinary.com/dbbuav0rj/image/upload/v1729496075/Portfolio/images/logo_infmovilweb_tafnul.webp'} alt="Company Logo" width={40} height={40}/>
                     <span>Infmovilweb</span>
                 </div>
             </div>
