0dfbc72d3c3d38a6d5c9e128b23d17b2dada5777
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-21 09:31:54 +0200

    [/] opinions: add new component

--- components/pages/Opinions/Opinions.page.tsx
+++ components/pages/Opinions/Opinions.page.tsx
@@ -1,24 +1,31 @@
+import Paragraph from "@/components/UI/Word/Paragraph.component";
 import s from "./Opinions.page.module.scss";
-
-import rec_1 from "@/assets/Rectangle_1.png";
-import companyLogo from "@/assets/Infmovilweb.jpeg";
-
 import Image from "next/image";
-import OpinionsBoxSection from "./OpinionsBoxSection/OpinionsBoxSection.component";
 
 function Opinions() {
   return (
-    <section id="opinions" className={s.container}>
-      <Image
-        height={700}
-        width={600}
-        src={rec_1}
-        alt="Reactangel"
-        className={s.container__rec1}
-      />
-      <section className={s.container__boxWrapper}>
-       <OpinionsBoxSection companyLogo={companyLogo}/>
-      </section>
+    <section className={s.container}>
+      <div className={s.container__sectionInfoWrapper}>
+        <span className={s.container__sectionInfoWrapper__content}>O Mnie</span>
+      </div>
+      <div className={s.container__headerWrapper}>
+        <h3 className={s.container__headerWrapper__header}>
+          <Paragraph
+            className={s.container__headerWrapper__header}
+            value="Opinie Moich Klientów"
+          />
+        </h3>
+      </div>
+      <div className={s.container__opinionsWrapper}>
+            <div className={s.container__opinionsWrapper__opinion}>
+                <p className={s.container__opinionsWrapper__opinion__text}></p>
+                <a href="" target="_blank" rel="noopener noreferrer"><span className={s.container__importantTextYtDecorate}>Zobacz Pełną Opinię</span></a>
+                <div className={s.container__opinionsWrapper__opinion__imgWrapper}>
+                    <Image src={''} alt="Company Logo" width={400} height={300}/>
+                    <span>Infmovilweb</span>
+                </div>
+            </div>
+      </div>
     </section>
   );
 }
