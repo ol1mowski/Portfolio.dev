a0c086c02cdc64fafad2c94101a108d9843c5980
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-22 10:04:34 +0200

    [/] Opinions: add second opinion

--- components/pages/Opinions/Opinions.page.tsx
+++ components/pages/Opinions/Opinions.page.tsx
@@ -18,14 +18,58 @@ function Opinions() {
         </h3>
       </div>
       <div className={s.container__opinionsWrapper}>
-            <div className={s.container__opinionsWrapper__opinion}>
-                <p className={s.container__opinionsWrapper__opinion__text}>Oliwier Markiewicz is a good lad and very hardworking. He make us the company website as we instructed. We hope you do well in life.</p>
-                <a href="https://jumpshare.com/s/mNlcSWPwdNWRNFOaei2n" target="_blank" rel="noopener noreferrer"><span className={s.container__importantTextDecorate}>Zobacz Pełną Opinię</span></a>
-                <div className={s.container__opinionsWrapper__opinion__imgWrapper}>
-                    <Image src={'https://res.cloudinary.com/dbbuav0rj/image/upload/v1729496075/Portfolio/images/logo_infmovilweb_tafnul.webp'} alt="Company Logo" width={40} height={40}/>
-                    <span>Infmovilweb</span>
-                </div>
-            </div>
+        <div className={s.container__opinionsWrapper__opinion}>
+          <p className={s.container__opinionsWrapper__opinion__text}>
+            Oliwier Markiewicz is a good lad and very hardworking. He make us
+            the company website as we instructed. We hope you do well in life.
+          </p>
+          <a
+            href="https://jumpshare.com/s/mNlcSWPwdNWRNFOaei2n"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className={s.container__importantTextDecorate}>
+              Zobacz Pełną Opinię
+            </span>
+          </a>
+          <div className={s.container__opinionsWrapper__opinion__imgWrapper}>
+            <Image
+              src={
+                "https://res.cloudinary.com/dbbuav0rj/image/upload/v1729496075/Portfolio/images/logo_infmovilweb_tafnul.webp"
+              }
+              alt="Company Logo"
+              width={40}
+              height={40}
+            />
+            <span>Infmovilweb</span>
+          </div>
+        </div>
+        <div className={s.container__opinionsWrapper__opinion}>
+          <p className={s.container__opinionsWrapper__opinion__text}>
+            Oliwier Markiewicz is a good lad and very hardworking. He make us
+            the company website as we instructed. We hope you do well in life.
+          </p>
+          <a
+            href="https://jumpshare.com/s/mNlcSWPwdNWRNFOaei2n"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className={s.container__importantTextDecorate}>
+              Zobacz Pełną Opinię
+            </span>
+          </a>
+          <div className={s.container__opinionsWrapper__opinion__imgWrapper}>
+            <Image
+              src={
+                "https://res.cloudinary.com/dbbuav0rj/image/upload/v1729584212/Portfolio/Icons/apple-touch-icon_fezbnv.png"
+              }
+              alt="Company Logo"
+              width={40}
+              height={40}
+            />
+            <span>Troska</span>
+          </div>
+        </div>
       </div>
     </section>
   );
