516060b68422d19f55f1316fdb81add38de3b908
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-26 13:42:52 +0200

    [/]: Hero page: refactopring code

--- components/pages/HomePage/HomePage.page.tsx
+++ components/pages/HomePage/HomePage.page.tsx
@@ -1,20 +1,20 @@
-import s from "./HomePage.page.module.scss";
-
 import TechStack from "../../UI/techStack/techStack.component";
 import HomePageInfo from "../../UI/HomePageInfo/HomePageInfoSection.component";
 
-import img from "@/assets/new_main.svg";
 import HomePageImageSection from "../../UI/HomePageImageSection/HomePageImageSection.component";
+import HeroSectionWrapper from "./HeroSectionWrapper/HeroSectionWrapper.component";
+import MainSectionWrapper from "./MainSectionWrapper/MainSectionWrapper.component";
 
 function HomePage() {
   return (
-    <section id="home" className={s.homeContainer}>
-      <section className={s.homeContainer__infoSection}>
-        <HomePageImageSection img={img} />
+    <HeroSectionWrapper>
+      <MainSectionWrapper>
+        <HomePageImageSection />
         <HomePageInfo />
-      </section>
+      </MainSectionWrapper>
+
       <TechStack />
-    </section>
+    </HeroSectionWrapper>
   );
 }
 
