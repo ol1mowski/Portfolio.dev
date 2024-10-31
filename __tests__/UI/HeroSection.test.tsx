7963af700119f55b5c582f491bf2d22ce8e7f2b0
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-31 09:46:13 +0100

    [/]: __tests__: configure JEST nad write first UI test

--- __tests__/UI/HeroSection.test.tsx
+++ __tests__/UI/HeroSection.test.tsx
@@ -1,13 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import HeroCta from '@/components/pages/HomePage/HomePageInfo/HeroCta/HeroCta.component';
-import '@testing-library/jest-dom';
+import HeroCta from '../../components/pages/HomePage/HomePageInfo/HeroCta/HeroCta.component';
 
 describe('HeroCta', () => {
   test('renders the correct text and button', () => {
-    render(<HeroCta />);
-
-    const textElement = screen.getByText(/Cześć! Nazywam się Oliwier Markiewicz/i);
-    expect(textElement).toBeInTheDocument();
+    render(<HeroCta />); 
 
     const buttonElement = screen.getByRole('button', { name: /Zwiększ Swoją Sprzedaż/i });
     expect(buttonElement).toBeInTheDocument();
