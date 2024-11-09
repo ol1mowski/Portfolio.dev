1c8cbf8561899ba117c8aa4b209f56a5d1c0034e
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-11-09 22:38:36 +0100

    [/] Header: resolve problem with no woriking menu links

--- components/pages/Header/NavBar/NavBar.component.tsx
+++ components/pages/Header/NavBar/NavBar.component.tsx
@@ -1,25 +1,17 @@
 import { memo } from 'react';
 import Link from 'next/link';
 import s from "./NavBar.component.module.scss";
-
-const NAV_ITEMS = [
-  { href: '/#home', label: 'Home' },
-  { href: '/#about', label: 'O Mnie' },
-  { href: '/#projects', label: 'Projekty' },
-  { href: '/Blog', label: 'Blog' },
-  { href: '/#opinions', label: 'Opinie' },
-  { href: '/#contact', label: 'Kontakt' }
-];
+import { menuItems } from '@/data/MenuItems.data';
 
 const NavBar = memo(() => {
   return (
     <nav className={s.navSectionBig}>
-      {NAV_ITEMS.map(({ href, label }) => (
+      {menuItems.map(({ to, label }) => (
         <Link 
-          key={href}
-          href={href}
+          key={to}
+          href={to}
           className={s.navSectionBig__item}
-          scroll={!href.startsWith('/#')}
+          scroll={!to.startsWith('/#')}
         >
           {label}
         </Link>
