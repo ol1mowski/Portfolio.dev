055cc8851601102a71ff081fc885776a49668b64
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-11-09 22:27:55 +0100

    [/] Header: delete unnecesarry button element in showMobileMenu component

--- components/pages/Header/ShowMobileMenu/ShowMobileMenu.component.tsx
+++ components/pages/Header/ShowMobileMenu/ShowMobileMenu.component.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import { memo, useCallback } from 'react';
+import { memo, useCallback } from "react";
 import Image from "next/image";
 import s from "./ShowMobileMenu.component.module.scss";
 import { useContext } from "react";
@@ -17,29 +17,22 @@ const ShowMobileMenu = memo(() => {
   return (
     <>
       <nav className={s.navSection} data-testid="mobile-nav">
-        <button 
-          className={s.navSection__icon}
+        <Image
+          src="https://res.cloudinary.com/dbbuav0rj/image/upload/v1729846726/Portfolio/Icons/hamburger_black_cyojcu.svg"
+          alt="Menu icon"
           onClick={handleOpen}
           aria-label="Open menu"
-          data-testid="hamburger-button"
-        >
-          <Image
-            src="https://res.cloudinary.com/dbbuav0rj/image/upload/v1729846726/Portfolio/Icons/hamburger_black_cyojcu.svg"
-            alt="Menu icon"
-            width={30}
-            height={30}
-            className={s.navSection__icon__img}
-            data-testid="hamburger-icon"
-          />
-        </button>
+          data-testid="hamburger-icon"
+          width={30}
+          height={30}
+          className={s.navSection__icon}
+        />
       </nav>
-      {isOpen && (
-        <MobileMenuHeader data-testid="mobile-menu" />
-      )}
+      {isOpen && <MobileMenuHeader data-testid="mobile-menu" />}
     </>
   );
 });
 
-ShowMobileMenu.displayName = 'ShowMobileMenu';
+ShowMobileMenu.displayName = "ShowMobileMenu";
 
 export default ShowMobileMenu;
