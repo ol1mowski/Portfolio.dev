9c5d5787c72f90f8fbec6db93ea9e883fbd81bfe
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-11-10 21:45:56 +0100

    [/] HeroSection: optymizing components add useMemo Hook

--- components/pages/HomePage/HomePageInfo/HeroCta/HeroCta.component.tsx
+++ components/pages/HomePage/HomePageInfo/HeroCta/HeroCta.component.tsx
@@ -1,18 +1,28 @@
-import Text from "@/components/Utils/Text-component/Text.component";
 import s from "./HeroCta.component.module.scss";
+
+import { memo } from 'react';
+import Link from 'next/link';
+
+import Text from "@/components/Utils/Text-component/Text.component";
 import Button from "@/components/UI/Button/Button.component";
 
-function HeroCta() {
+const HeroCta = memo(() => {
   return (
     <section className={s.cta}>
       <p className={s.cta__p}>
         <Text textValue="Cześć! Nazywam się Oliwier Markiewicz, pomagam lokalnym firmom zwiększyć sprzedaż poprzez tworzenie nowoczesnych stron internetowych, które wyróżniają się na tle konkurencji 🏆" />
       </p>
-      <a href="#services">
-        <Button type="normal" value="Zwiększ Swoją Sprzedaż" />
-      </a>
+      <Link href="#services">
+        <Button 
+          type="normal" 
+          value="Zwiększ Swoją Sprzedaż"
+          aria-label="Przejdź do sekcji usług"
+        />
+      </Link>
     </section>
   );
-}
+});
+
+HeroCta.displayName = 'HeroCta';
 
 export default HeroCta;
--- components/pages/HomePage/TechStack/TechStack.component.tsx
+++ components/pages/HomePage/TechStack/TechStack.component.tsx
@@ -1,28 +1,32 @@
+import { memo } from 'react';
 import s from "./techStack.module.scss";
-
 import { SKILLS } from "@/data/Skills.data";
 import Tech from "./Tech/Tech.component";
 
-const TechStack = () => {
+const TechStack = memo(() => {
   return (
-    <>
-      <section className={s.techStackContainer}>
-        <div className={s.techStackContainer__title}>
-          <p className={s.techStackContainer__title__p}>Narzędzia</p>
-        </div>
-        <div className={s.techStackContainer__skillsWrapper}>
-          {SKILLS.map((skill) => (
-            <Tech
-              key={skill.id}
-              src={skill.src}
-              href={skill.href}
-              alt={skill.alt}
-            />
-          ))}
-        </div>
-      </section>
-    </>
+    <section 
+      className={s.techStackContainer}
+      aria-label="Technologie i narzędzia"
+    >
+      <div className={s.techStackContainer__title}>
+        <p className={s.techStackContainer__title__p}>Narzędzia</p>
+      </div>
+      <div 
+        className={s.techStackContainer__skillsWrapper}
+        role="list"
+      >
+        {SKILLS.map((skill) => (
+          <Tech
+            key={skill.id}
+            {...skill}
+          />
+        ))}
+      </div>
+    </section>
   );
-};
+});
+
+TechStack.displayName = 'TechStack';
 
 export default TechStack;
