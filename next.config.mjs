3dd034e1d199119dd87d5622103c40c93dfc791c
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-12-04 21:06:47 +0100

    emergency repair save user in db

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: "res.cloudinary.com",
-      },
-    ],
+    domains: ['res.cloudinary.com'],
   },
   env: {
     DB_URL: process.env.DB_URL,
     JWT_SECRET: process.env.JWT_SECRET,
+  },
+  productionBrowserSourceMaps: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
   },
 };
 
