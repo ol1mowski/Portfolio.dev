5e1acb4022422b1f94cc331d30905b5b94489e43
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-12-04 08:00:36 +0100

    Header: add link to home page to logo]

--- components/pages/Header/Logo/HeaderLogo.component.tsx
+++ components/pages/Header/Logo/HeaderLogo.component.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link";
 import s from "./HeaderLogo.component.module.scss";
 
 export default function HeaderLogo() {
   return (
     <section className={s.logoSection}>
-      <h3 className={s.logoSection__content}>
-        <span className={s.logoSection__content__name}>
-          Oliwier
-        </span>
-        .dev
-      </h3>
+      <Link href="/">
+        <h3 className={s.logoSection__content}>
+          <span className={s.logoSection__content__name}>Oliwier</span>
+          .dev
+        </h3>
+      </Link>
     </section>
   );
 }
