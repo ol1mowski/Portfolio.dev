3d4fba92130d9d0a991367e2f81c4bad3e8c898b
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-12-06 19:40:03 +0100

    AboutMe: KeySkills - change next js to react

--- data/Skills.data.ts
+++ data/Skills.data.ts
@@ -1,5 +1,36 @@
 import { type Skill } from "@/types/Skills.type";
 
+export const KEY_TECHNOLOGIES = [
+  {
+    id: "react",
+    alt: "react icon",
+    src: "https://res.cloudinary.com/dbbuav0rj/image/upload/v1729949256/Portfolio/Icons/react_kedjpd.svg",
+    width: 40,
+    height: 40,
+  }, 
+  {
+    id: "typescript",
+    alt: "typescript icon",
+    src: "https://res.cloudinary.com/dbbuav0rj/image/upload/v1729949257/Portfolio/Icons/typescript_cwy8os.svg",
+    width: 40,
+    height: 40,
+  },
+  {
+    id: "webflow",
+    alt: "webflow icon",
+    src: "https://res.cloudinary.com/dbbuav0rj/image/upload/v1729949720/Portfolio/Icons/webflow_crbuyc.svg",
+    width: 40,
+    height: 40,
+  },
+  {
+    id: "figma",
+    alt: "figma icon",
+    src: "https://res.cloudinary.com/dbbuav0rj/image/upload/v1729949658/Portfolio/Icons/figma_tn9mnb.svg",
+    width: 40,
+    height: 40,
+  },
+]
+
 export const SKILLS: Skill[] = [
   {
     id: "figma",
