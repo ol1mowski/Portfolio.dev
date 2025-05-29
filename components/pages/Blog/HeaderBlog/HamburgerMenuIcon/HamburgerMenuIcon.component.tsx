c6bc914eb94aa6cde28bda037341f85e9432de3b
Ol1mowski <olimarki123@wp.pl>
2025-05-29 17:56:35 +0200

    fix: repair props conflict in header

--- components/pages/Blog/HeaderBlog/HamburgerMenuIcon/HamburgerMenuIcon.component.tsx
+++ components/pages/Blog/HeaderBlog/HamburgerMenuIcon/HamburgerMenuIcon.component.tsx
@@ -2,7 +2,11 @@ import HamburgerClickContext from '@/store/HamburgerClickContext';
 import { useContext } from 'react';
 import s from './HamburgerMenuIcon.component.module.scss';
 
-const HamburgerMenuIcon = () => {
+interface HamburgerMenuIconProps {
+  post?: boolean;
+}
+
+const HamburgerMenuIcon = ({ post }: HamburgerMenuIconProps) => {
   const { setOpen } = useContext(HamburgerClickContext);
 
   const openHamburgerMenuHandler = () => {
